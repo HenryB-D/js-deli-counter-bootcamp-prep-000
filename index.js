f20a9f39c74fe93fd2559eaa518f0dbb89f5cf2c
Henry Brooke-Dean <henrybrookedean@hotmail.com>
2018-10-23 09:09:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeliLine = [];
   
 function takeANumber(katzDeliLine) {
   katzDeliLine.push(1);
-  var katzDeliLine = katzdeliline (+1)
+  var katzDeliLine = katzDeliline (+1)
   return ('Welcome, ' + ". You are number "  + katzDeliLine.length  +  " in line.");
 }
 
